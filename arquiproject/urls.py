29064b6b961ac1c423bbc0d6ae53741821e09af7
Juan Alberto Jimenez <jualjiman@gmail.com>
2016-03-31 21:30:29 -0600

    Including facebook meta tags, and fixing pep8 in url and models files.

--- arquiproject/urls.py
+++ arquiproject/urls.py
@@ -1,48 +1,44 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-from blog import views
-#from django.views.generic import TemplateView
-from django.views.generic import ListView,DetailView
-from blog.views import *
 from django.conf import settings
+from django.conf.urls import include, patterns, url
+from django.contrib import admin
+
+from blog.views import *
 
 
-
-urlpatterns = patterns('',
-    # Examples:
-    #url(r'^$', 'arquiproject.views.home', name='home'),
-   # url(r'^$', include('blog.urls')),
-
-    #url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
-   # INDEX
-   url(r'^$', 'blog.views.index', name='index'),
-   #NOTICIAS
-   url(r'^noticias/(?P<slug>[-\w]+)/$', (NoticiasView.as_view(template_name = "noticias.html"))),
-   #TODAS LAS NOTICIAS
-   url(r'^noticias-todas/$', (TodasNView.as_view(template_name = "noticias-todas.html"))),
-   #HISTORIA
-   url(r'^historia/$', (HistoriaView.as_view(template_name = "historia.html"))),
-   #MISION-VISION
-   url(r'^mision-vision/$', (MisionVisionView.as_view(template_name = "mision-vision.html"))),
-   #PERFIN DE INGRESO Y EGRESO
-   url(r'^perfil-ingreso/$', (PerfilIngresoView.as_view(template_name = "perfil-ingreso.html"))),
-   #ACREDITACION
-   url(r'^acreditacion/$', (AcreditacionView.as_view(template_name = "acreditacion.html"))),
-   #DOCENTES
-   url(r'^docentes/$', (DocentesView.as_view(template_name = "construccion.html"))),
-    
-   url(r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_ROOT} ),
-   #CKEDITOR
-	(r'^ckeditor/', include('ckeditor.urls')),
-
-
-	
-
-
-
-    #url(r'^noticias/(\d+)/$', 'blog.views.noticias', name='noticias'),
-    )
-handler404='blog.views.e404'
-
+    # INDEX
+    url(r'^$', 'blog.views.index', name='index'),
+    # NOTICIAS
+    url(r'^noticias/(?P<slug>[-\w]+)/$', (NoticiasView.as_view(template_name="noticias.html"))),
+    # TODAS LAS NOTICIAS
+    url(r'^noticias-todas/$', (TodasNView.as_view(template_name="noticias-todas.html"))),
+    # HISTORIA
+    url(r'^historia/$', (HistoriaView.as_view(template_name="historia.html"))),
+    # MISION-VISION
+    url(r'^mision-vision/$', (MisionVisionView.as_view(template_name="mision-vision.html"))),
+    # PERFIN DE INGRESO Y EGRESO
+    url(r'^perfil-ingreso/$', (PerfilIngresoView.as_view(template_name="perfil-ingreso.html"))),
+    # ACREDITACION
+    url(r'^acreditacion/$', (AcreditacionView.as_view(template_name="acreditacion.html"))),
+    # DOCENTES
+    url(r'^docentes/$', (DocentesView.as_view(template_name="construccion.html"))),
+    url(
+        r'^static/(?P<path>.*)$',
+        'django.views.static.serve',
+        {
+            'document_root': settings.STATIC_ROOT
+        }
+    ),
+    url(
+        r'^media/(?P<path>.*)$',
+        'django.views.static.serve',
+        {
+            'document_root': settings.MEDIA_ROOT
+        }
+    ),
+    # CKEDITOR
+    url(r'^ckeditor/', include('ckeditor.urls')),
+)
+handler404 = 'blog.views.e404'
