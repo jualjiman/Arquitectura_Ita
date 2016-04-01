fa34b521b13ab206fce8e6eb3999c4e8fb617d5b
Juan Alberto Jimenez <jualjiman@gmail.com>
2016-03-31 21:30:29 -0600

    Including facebook meta tags, and fixing pep8 in url and models files.

--- blog/models.py
+++ blog/models.py
@@ -1,53 +1,62 @@
-from django.db import models 
-from django.contrib import admin
-from django.template.defaultfilters import slugify
 from ckeditor.fields import RichTextField
 
-
-#from tinymce import models as tinymce_models
 from django.conf import global_settings
+from django.db import models
+from django.template.defaultfilters import slugify
+
+
 TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
     'django.core.context_processors.request',
 )
 BOOTSTRAP_ADMIN_SIDEBAR_MENU = True
 
+
 # Create your models here.)
 class Entrada(models.Model):
-	imagen = models.ImageField(upload_to = 'imagenes',help_text="Imagen de la noticia")
-	titulo = models.CharField(max_length=100,help_text="Titulo de la noticia maximo 100 caracteres")
-	resumen = models.CharField(max_length=170,help_text="Resumen de la noticia maximo 170 caracteres")
-	contenido = RichTextField()
-	time = models.DateTimeField()
-	slug = models.SlugField(editable=False)
-	#descripcionArchivo = models.CharField(max_length=60, help_text="Texto descriptivo del archivo.")
-	#archivopdf = models.FileField(upload_to = "pdfpublicacion", help_text='Archivo asociado con la publicacion')
+    imagen = models.ImageField(
+        upload_to='imagenes',
+        help_text="Imagen de la noticia"
+    )
+    titulo = models.CharField(
+        max_length=100,
+        help_text="Titulo de la noticia maximo 100 caracteres"
+    )
+    resumen = models.CharField(
+        max_length=170,
+        help_text="Resumen de la noticia maximo 170 caracteres"
+    )
+    contenido = RichTextField()
+    time = models.DateTimeField()
+    slug = models.SlugField(editable=False)
+
+    class Meta:
+        ordering = ['-time']
+
+    def __unicode__(self):
+            return self.titulo
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.titulo)
+        super(Entrada, self).save(*args, **kwargs)
 
 
-	
-	
-
-	class Meta:
-		ordering = ['-time']
-
-	def __unicode__(self):
-		return self.titulo
-
-	def save(self, *args, **kwargs):
-		if not self.id:
-			self.slug = slugify(self.titulo)
-		super(Entrada, self).save( *args, **kwargs)
-
-	
-	
 class Slider(models.Model):
-	imagen = models.ImageField(upload_to= 'noticias',help_text="Imagen que sera mostrada")
-	titulo = models.CharField(max_length=50,help_text="Titulo que sera mostrado en la imagen, maximo 50 caracteres")
-	activo = models.BooleanField(default=True, help_text="Mostrar o no la imagen con texto")
+    imagen = models.ImageField(
+        upload_to='noticias',
+        help_text="Imagen que sera mostrada"
+    )
+    titulo = models.CharField(
+        max_length=50,
+        help_text="Titulo que sera mostrado en la imagen, maximo 50 caracteres"
+    )
+    activo = models.BooleanField(
+        default=True,
+        help_text="Mostrar o no la imagen con texto"
+    )
 
-	def __unicode__(self):
-		return self.titulo
-	
-	
+    def __unicode__(self):
+        return self.titulo
 
 
 
