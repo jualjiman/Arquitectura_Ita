07b4459b8183c766376fb4935d0b8cb8634958ce
Juan Alberto Jimenez Angel <jualjiman@gmail.com>
2016-04-03 19:03:59 -0500

    settings fixes

--- arquiproject/settings.py
+++ arquiproject/settings.py
@@ -17,17 +17,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xha+p33!o#fw=ih#^=$osasl+k^7hx+zy4g0n+%i7rqaykt$_t'
+SECRET_KEY = 'xha+p33!o#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = [
-    '.ita-arquitectura.com.',
-    '.ita-arquitectura.com',
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
