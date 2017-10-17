c1c4538ba44b9e4e6c9770b9e74afb0ce4975f85
Juan Alberto Jimenez Angel <jualjiman@gmail.com>
2017-10-17 08:57:32 -0500

    Adding production assets

---
+++ arquiproject/local_settings.py
@@ -0,0 +1,15 @@
+SECRET_KEY = 'xha+p33!o#fw=ih#^=$osasl+k^7hx+zy4g0n+%i7rqaykt$_t'
+DATABASES = {
+    'default': {
+        'NAME': 'arqita',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'wadmin',
+        'PASSWORD': '081510979s'
+    }
+  }
+DEBUG = False
+THUMBNAIL_DEBUG = DEBUG
+ALLOWED_HOSTS = [
+    '.ita-arquitectura.com.',
+    '.ita-arquitectura.com',
+]
